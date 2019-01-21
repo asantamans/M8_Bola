4343160b0829f2c9524424ffb232cae8b9813940
Arnau <arnau.santamans@gmail.com>
2019-01-21 20:25:09 +0100

    Implementacio multiples pilotes Implementacio layout dinamic Fa falta afinar el rebot i la colisio

--- app/src/main/java/com/example/tnb_20/m8_bola/MainActivity.java
+++ app/src/main/java/com/example/tnb_20/m8_bola/MainActivity.java
@@ -1,130 +1,74 @@
 package com.example.tnb_20.m8_bola;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-import java.util.ArrayList;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
-    /*
-    Funcionament del codi similar a MainActivityOld
-    que es la app amb una pilota rebotant per la pantalla
-     */
-    private ArrayList<Pilota> arrayPilotes;
-    //Nota: Serveix epr a obtenir les dimensions de la pantalla App i que no es surti la pilota
-    private int status;
-    private DisplayMetrics displayMetrics;
 
+public class MainActivity extends AppCompatActivity {
+   public static Pilota[] arrayPilotes;
+    public static int statusBar;
+    public static int ample;
+    public static int alt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.displayMetrics = this.getBaseContext().getResources().getDisplayMetrics();
-        this.status = getResources().getDimensionPixelSize(getResources().getIdentifier("status_bar_height", "dimen", "android"));
+        RelativeLayout layout = findViewById(R.id.pantalla);
+        statusBar = getResources().getDimensionPixelSize(getResources().getIdentifier("status_bar_height", "dimen", "android")); // Calculem la mida de la pantalla per a que les pilotes no surtin dels marges
+        DisplayMetrics display = this.getBaseContext().getResources().getDisplayMetrics();
+        ample = display.widthPixels;//Ample de la pantalla
+        alt = display.heightPixels; //Alt de la pantalla
 
-        renderitzarPilotes();
+        //Coloquem les 4 pilotes generades en posicions preestablertes de inici tal que quedin dispersades unes de les altres
+        float[] posX= {alt/10*1, alt/10*2, alt/10*4, alt/10*8};
+        float[] posY = {ample/10*1, ample/10*2, ample/10*4, ample/10*8};
 
+        //Posem una velocitat a les pilotes (Es poden posar mes velocitats
+        float[] vel = {2, -2};
+        arrayPilotes = new Pilota[4];//Creem un array de 4 pilotes; Falta implementar demanar per pantalla cuantes pilotes vol l'usuari
+        for (int i = 0; i<4 ; i++) {
+            //Randomitzem la velocitat de les pilotes en els vectors X i Y
+            Float tmpVelX = vel[new Random().nextInt(2)];
+            Float tmpVelY =  vel[new Random().nextInt(2)];
+            arrayPilotes[i]=new Pilota(tmpVelX,tmpVelY, i);
+            arrayPilotes[i].pilotaView = new ImageView(getApplicationContext());//Asignem un imageView a la pilota
+            layout.addView(arrayPilotes[i].pilotaView);//Asignem la imatge al layout dinamic
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) arrayPilotes[i].pilotaView.getLayoutParams();
+            params.width = 120;
+            params.height = 120;
+            arrayPilotes[i].pilotaView.setLayoutParams(params);
+
+            arrayPilotes[i].pilotaView.setImageResource(R.drawable.bola);//Asignem la imatge a la pilota
+            //Randomitzem la posició d'inici de la pilota,caldria veure la probabilitat de que spawnejin 2 pilotes al mateix lloc
+            Float tmpPosX = posY[new Random().nextInt(4)];
+            Float tmpPosY = posX[i];
+            arrayPilotes[i].pilotaView.setX(tmpPosX);
+            arrayPilotes[i].pilotaView.setY(tmpPosY);
+        }
+
+        //Movem les pilotes de manera continua establint una tasa de refresc
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
-
-                mourePilota();
+                for(int i =0; i<4; i++) {
+                    arrayPilotes[i].mourePilotes(2, 2);
+                }
             }
         };
-        //Refresc de la pantalla per actualitzar el moviment de les pilotes
+
         Timer timer = new Timer();
-        timer.schedule(task, 100, 30);
-    }
-    private void renderitzarPilotes() {
-        arrayPilotes = new ArrayList<>();
-
-       //Pilota 1
-        Pilota bola = new Pilota();
-        bola.setDespX(20.0);
-        bola.setDespY(20.0);
-        bola.setX(4);
-        bola.setY(4);
-        bola.setPilota((ImageView) findViewById(R.id.pilota1));
-        bola.setAmple(this.displayMetrics.widthPixels);
-        bola.setAlt(this.displayMetrics.heightPixels);
-
-        //Pilota2
-        Pilota bola2 = new Pilota();
-        bola2.setDespX(8.0);
-        bola2.setDespY(8.0);
-        bola2.setX(2);
-        bola2.setY(2);
-        bola2.setPilota((ImageView) findViewById(R.id.pilota1));
-        bola2.setAmple(this.displayMetrics.widthPixels);
-        bola2.setAlt(this.displayMetrics.heightPixels);
-
-        arrayPilotes.add(bola);
-        arrayPilotes.add(bola2);
+        timer.schedule(task, 1000,30);
     }
 
-    private void mourePilota() {
-        for (Pilota pilota: arrayPilotes) {
-            double positionX = pilota.getX() + (pilota.getX() * pilota.getDespX());
-            double positionY = pilota.getY() + (pilota.getY() * pilota.getDespY());
-
-    compPosX(pilota, positionX);
-    compPosY(pilota, positionY);
-
-    calcularPosCreuament();
-}
-    }
-
-private void calcularPosCreuament(){
-        // Calculem la nova posicio a la que es desplaçaran les pilotes a rao de invertir el eix x i y per simular que xoquen (faltarien variables de pes per a mes realisme)
-        if(arrayPilotes.get(0).getX() >= arrayPilotes.get(1).getX() || arrayPilotes.get(0).getX() <= arrayPilotes.get(1).getX() + arrayPilotes.get(1).getAmple()){
-        arrayPilotes.get(0).setDespX(arrayPilotes.get(0).getDespX() * (-1));
-        arrayPilotes.get(1).setDespX(arrayPilotes.get(1).getDespX() * (-1));
-        }
-        }
-
-
-        //FUncions per a recalcular la posicioX i Y de la pilota
-private void compPosX(Pilota pilota, double posicioX){
-        if(pilota.getX() > 0){
-        if(posicioX + pilota.getAmple() < pilota.getAmple()){
-        pilota.setX((float) posicioX);
-        }else{
-        pilota.setX((float) pilota.getAmple() - pilota.getAmple());
-        pilota.setDespX(pilota.getDespX() * (-1));
-        }
-        }else{
-        if (posicioX > 0) {
-        pilota.setX((double) posicioX);
-        } else{
-        pilota.setX(0);
-        pilota.setDespX(pilota.getDespX() * (-1));
-        }
-        }
-        }
-
-private void compPosY(Pilota pilota, double posicioY) {
-        if (pilota.getY() > 0) {
-        if (posicioY + pilota.getAlt() + this.status < pilota.getAlt()) {
-        pilota.setY((double) posicioY);
-        } else {
-        pilota.setY((double) pilota.getAlt() - pilota.getAlt() - this.status);
-        pilota.setDespY(pilota.getDespY() * (-1));
-        }
-        } else {
-        if (posicioY > 0) {
-        pilota.setY((double) posicioY);
-        } else {
-        pilota.setY(0);
-        pilota.setDespY(pilota.getDespY() * (-1));
-        }
-        }
-        }
 
 
 }
-
--- app/src/main/java/com/example/tnb_20/m8_bola/Pilota.java
+++ app/src/main/java/com/example/tnb_20/m8_bola/Pilota.java
@@ -1,76 +1,125 @@
 package com.example.tnb_20.m8_bola;
 
-
 import android.widget.ImageView;
 
 public class Pilota {
-    private ImageView pilota;
-    private int ample;
-    private int alt;
-    private double x;
-    private double y;
-    private double despX;
-    private double despY;
+    int pilotaId;//Identificador unic de les pilotes per a identificar quan colisiona amb una altra diferent
+    ImageView pilotaView;
+    float velX;
+    float velY;
 
 
-    //Generat tot de manera automatica
-    public Pilota(){
-
+    //generat automaticament
+    public Pilota(float velX, float velY, int id){
+        this.velX = velX;
+        this.velY = velY;
+        this.pilotaId = id;
     }
 
-    public ImageView getPilota() {
-        return pilota;
+    //Funcio per calcular moviment de les pilotes dins la pantalla SENSE colisions, en cas de colisio, veure xocPilota
+    public void mourePilotes(float x, float y) {
+        //Si X > 0, segons l'eix de coordenades es mou a la dreta,
+        //En cas contrari, es mou cap a l'esquerra
+
+
+        //EIX DE LES X
+        //Per obtenir la nova pos, es tan facil com pos = posX(Actual) + velX*x donat que no hi ha variable T, fem servir X
+        float recalculPosX = pilotaView.getX() + x * velX;
+        if ( 0 < pilotaView.getX() ) {
+            //Comprovacio per evitar que es surti de la pantalla la imatge i perdem una pilota
+            if ( MainActivity.ample > pilotaView.getWidth()+recalculPosX ) {
+                pilotaView.setX(recalculPosX);
+            } else {//La pilota surt de la pantalla per el calcul de moviment, reposicionem la coordenada X al limit de la pantalla
+                pilotaView.setX(MainActivity.ample - pilotaView.getWidth());
+                velX=(-1)*velX;//Important el -1, ja que aixi invertim la direccio i evitem que s'encalli al mateix eix, simula que rebota
+            }
+        } else {
+            //Mateix cas anterior explicat adalt, pero donat que X < 0, segons l'eix de coordenades es mou a l'esquerra
+            if ( 0< recalculPosX ) {
+                pilotaView.setX(recalculPosX);
+            } else{
+                //La coloquem a la posició 0 i fem que reboti; mateix cas anterior
+                pilotaView.setX(0);
+                velX=(-1)*velX;
+            }
+        }
+
+        //EIX DE LES Y
+        //Mateix cas anterior de l'eix X pero amb l'eix Y
+        //A diferencia del X, si Y>0 --> Es mou cap a dalt
+        //Si Y < 0 --> Es mou cap abaix
+        float recalculPosY = pilotaView.getY() + y * velY;
+        if ( 0 < pilotaView.getY() ) {
+            if ( MainActivity.alt > MainActivity.statusBar + recalculPosY + pilotaView.getHeight()  ) {//
+                pilotaView.setY(recalculPosY);
+            } else {
+                pilotaView.setY(MainActivity.alt - pilotaView.getHeight() - MainActivity.statusBar);
+                velY=(-1)*velY;
+            }
+        }else {
+            if ( 0 < recalculPosY) {
+                pilotaView.setY(recalculPosY);
+            } else {
+                pilotaView.setY(0);
+                velY=(-1)*velY;
+            }
+        }
+        //Un cop recalculat entre parets, recalculem el resultat amb Colisio
+        xocPilota(x, y);
     }
 
-    public int getAmple() {
-        return ample;
-    }
+    public void xocPilota(float x, float y){
+        //Per evitar que colisioni amb ella mateixa, tenen que ser dos pilotes diferents, es a dir, dos pilotesId diferents
+        for (Pilota Pilota : MainActivity.arrayPilotes) {
+            /*Mateixa logica anterior
+            SI: X>0-->Moviment cap a la dreta
+            Si X < 0 --> Moviment cap a l'esquerra
+            Si Y >0 --> Moviment cap a dalt
+            Si Y < 0 --> Moviment cap abaix
+            Segons l'eix d'abcisses
+             */
+            //EIX DE LES X
+            if (Pilota.pilotaId!=this.pilotaId){
+                float recalculPosX = this.pilotaView.getX() + x * velX;//Per obtenir la nova pos, es tan facil com pos = posX(Actual) + velX*x donat que no hi ha variable T, fem servir X
+                if (this.pilotaView.getX() < Pilota.pilotaView.getX()) {
+                    //Comprovacio per evitar que es surti de la pantalla la imatge i perdem una pilota
+                    if ( Pilota.pilotaView.getX()+Pilota.pilotaView.getWidth() < recalculPosX + this.pilotaView.getWidth()) {
+                        this.pilotaView.setX(recalculPosX);
+                    } else {
+                        //La pilota surt de la pantalla per el calcul de moviment, reposicionem la coordenada X al limit de la pantalla
+                        this.pilotaView.setX(MainActivity.ample - this.pilotaView.getWidth());
+                        velX=(-1)*velX;
+                    }
+                } else {// Fem el mateix pel moviment cap a l'esquerra
+                    if (recalculPosX < Pilota.pilotaView.getX()) {
+                        this.pilotaView.setX(recalculPosX);
+                    } else{
+                        this.pilotaView.setX(0);
+                        velX=(-1)*velX;//Important el -1, ja que aixi invertim la direccio i evitem que s'encalli al mateix eix, simula que rebota
+                    }
+                }
 
-    public int getAlt() {
-        return alt;
-    }
 
-    public double getX() {
-        return x;
-    }
 
-    public double getY() {
-        return y;
-    }
+                //Mateix cas anterior de l'eix X pero amb l'eix Y
+                float recalculPosY = this.pilotaView.getY() + y * velY;
+                if (this.pilotaView.getY() < Pilota.pilotaView.getY()) {
+                    if (  Pilota.pilotaView.getY()+Pilota.pilotaView.getHeight() < recalculPosY + this.pilotaView.getHeight() + MainActivity.statusBar ) {
+                        this.pilotaView.setY(recalculPosY);
+                    } else {
+                        this.pilotaView.setY(MainActivity.ample - this.pilotaView.getHeight() - MainActivity.statusBar);
+                        velY=(-1)*velY;//Important el -1
+                    }
+                }else {
+                    if (recalculPosY < Pilota.pilotaView.getY()) {
+                        this.pilotaView.setY(recalculPosY);
+                    } else {
+                        this.pilotaView.setY(0);
+                        velY=(-1)*velY;
+                    }
+                }
 
-    public double getDespX() {
-        return despX;
-    }
-
-    public double getDespY() {
-        return despY;
-    }
-
-    public void setPilota(ImageView pilota) {
-        this.pilota = pilota;
-    }
-
-    public void setAmple(int ample) {
-        this.ample = ample;
-    }
-
-    public void setAlt(int alt) {
-        this.alt = alt;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public void setDespX(double despX) {
-        this.despX = despX;
-    }
-
-    public void setDespY(double despY) {
-        this.despY = despY;
+            }
+        }
     }
 }
